9f456a8c59021375851cefa9f4e90ebdceb1036f
ChristophorusX <mrxrtxrt@gmail.com>
2018-02-23 19:37:29 -0500

    changes

--- belief_propagation.py
+++ belief_propagation.py
@@ -5,7 +5,7 @@ import sbm_generator as sbm
 def get_init_phi(n, q):
     """
     Returns a random initialization of all the messages of each pair of
-    vertices given they are in the same group of not.
+    vertices given they are in the same group or not.
 
     Parameters
     ----------
@@ -22,7 +22,11 @@ def get_init_phi(n, q):
     """
 
     phi = np.random.uniform(0, 1, (n, n, q))
+
     # phi = np.ones((n, n, q)) / q
+    # phi[1,1,0] = 1
+    # phi[1,1,1] = 0
+
     phi_empty = np.zeros((n, n, q))
 
     phi = normalize(phi)
@@ -31,12 +35,19 @@ def get_init_phi(n, q):
 
 
 def normalize(phi):
+    """
+    Returns a new tensor phi with probablities been normalized.
+    """
+
     n, _, q = phi.shape
-    phi_norm = 1 / np.sum(phi, axis=2)
-    normed_phi = np.zeros((n, n, q))
+    phi_norm = np.sum(phi, axis=2)
     for r in range(q):
-        normed_phi[:, :, r] = phi[:, :, r] * phi_norm
-    return normed_phi
+        # for i in range(n):
+        #     for j in range(n):
+        #         if phi_norm[i, j] != 0:
+        #             phi[i, j, r] = phi[i, j, r] / phi_norm[i, j]
+        phi[:, :, r] = phi[:, :, r] / phi_norm
+    return phi
 
 
 def edge_prob_mat(p_in, p_out):
@@ -72,6 +83,11 @@ def select_model(model, n, a, b):
 
 
 def belief_propagation(A, phi, phi_empty, p, n_repeat):
+    """
+    Belief propagation algorithm on observation A that returns a propogated
+    tensor phi.
+    """
+
     n, _, q = phi.shape
     for iteration in range(n_repeat):
         for i in range(n):
@@ -83,9 +99,7 @@ def belief_propagation(A, phi, phi_empty, p, n_repeat):
                             summation = phi[k, i, :].dot(p[r, :])
                             prod *= summation
                     phi_empty[i, j, r] = prod
-        temp = phi[:, :, :]
         phi = phi_empty[:, :, :]
-        phi_empty = temp[:, :, :]
 
         phi = normalize(phi)
 
@@ -93,6 +107,11 @@ def belief_propagation(A, phi, phi_empty, p, n_repeat):
 
 
 def marginal_prob(A, phi, p):
+    """
+    Returns a matrix of marginal probability of being in some cluster
+    for each vertex.
+    """
+
     n, _, q = phi.shape
     marginal = np.empty((n, q))
 
@@ -124,19 +143,19 @@ def marginal_prob(A, phi, p):
 
 
 if __name__ == "__main__":
-    n = 10
+    n = 50
     q = 2
-    a = .8
-    b = .3
+    a = .9
+    b = .1
     A, ground_truth, p_in, p_out = select_model("const-sbm", n, a, b)
+    # print("The observation is: \n{}".format(A))
     p = edge_prob_mat(p_in, p_out)
 
     phi_init, phi_empty = get_init_phi(n, q)
+    # print("Init Phi is: \n{}".format(phi_init[:, :, 0]))
     phi = belief_propagation(A, phi_init, phi_empty, p, 100)
     marginal = marginal_prob(A, phi, p)
 
-    print("The observation is: \n{}".format(A))
-    print("Init Phi is: \n{}".format(phi_init[:, :, 0]))
-    print("The propagation result is: \n{}".format(phi[:, :, 0]))
+    # print("The propagation result is: \n{}".format(phi[:, :, 0]))
     print("The marginal from BP is: \n{}".format(marginal))
     print("The ground truth is: \n{}".format(ground_truth))
