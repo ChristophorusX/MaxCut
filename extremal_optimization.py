e1a58c0c37e117e90ad356a09e6ffc3b83942960
ChristophorusX <mrxrtxrt@gmail.com>
2018-03-05 14:31:05 -0500

    update eo algorithm

--- extremal_optimization.py
+++ extremal_optimization.py
@@ -1,5 +1,8 @@
 import numpy as np
 import re
+import heapq
+from random import randint
+import scipy.stats as stats
 import propagation_node as ppn
 import propagation_graph as ppg
 
@@ -13,19 +16,22 @@ class EONode(ppn.Node):
         super(EONode, self).__init__(nid)
         self.name = name
         self.dim = dim
-        self.incoming = np.random.uniform(0, 1, (n, self.dim))
-        self.outgoing = np.random.uniform(0, 1, (n, self.dim))
-        self.oldoutgoing = np.random.uniform(0, 1, (n, self.dim))
+        self.incoming = None
+        self.outgoing = None
+        self.oldoutgoing = None
         self.observed = -1  # only >= 0 if variable is observed
         self.fitness = 0
         self.grouping = None
 
+    def __lt__(self, other):
+        return self.fitness < other.fitness
+
     def reset(self):
         super(EONode, self).reset()
-        n, q = self.incoming.shape
-        self.incoming = np.ones((n, self.dim))
-        self.outgoing = np.ones((n, self.dim))
-        self.oldoutgoing = np.ones((n, self.dim))
+        # n, q = self.incoming.shape
+        self.incoming = None
+        self.outgoing = None
+        self.oldoutgoing = None
         self.observed = -1
         self.fitness = 0
         self.grouping = None
@@ -68,6 +74,7 @@ class EOGraph(ppg.Graph):
 
     def __init__(self, adjacency, n_clusters, p):
         super(EOGraph, self).__init__(adjacency, n_clusters, p)
+        self.heap = None
 
     def add_eo_node(self, n, name, dim):
         newId = len(self.var)
@@ -81,10 +88,11 @@ class EOGraph(ppg.Graph):
         """
         Constructs the eograph from the given adjacency matrix.
         """
-        
+
         n, _ = self.adjacency.shape
         for i in range(n):
-            n_nbrs = int(np.linalg.norm(self.adjacency[i], ord=1) - 1) # we assume diagonals being 1
+            # we assume diagonals being 1
+            n_nbrs = int(np.linalg.norm(self.adjacency[i], ord=1) - 1)
             name = "Node #{}".format(i + 1)
             self.add_eo_node(n_nbrs, name, self.n_clusters)
 
@@ -92,4 +100,93 @@ class EOGraph(ppg.Graph):
             v.add_nbrs(self)
 
     def initialize_grouping(self):
-        pass
+        """
+        Randomly assigns the grouping to each node.
+        """
+
+        n, _ = self.adjacency.shape
+        grouping1 = 0
+        grouping2 = 0
+        for key, node in self.var.items():
+            if grouping1 < int(n / 2) and grouping2 < int(n / 2):
+                node.grouping = randint(1, 2)
+                if node.grouping == 1:
+                    grouping1 += 1
+                else:
+                    grouping2 += 1
+            elif grouping1 >= int(n / 2):
+                node.grouping = 2
+                grouping2 += 1
+            else:
+                node.grouping = 1
+                grouping1 += 1
+
+    def sorting_by_fitness(self):
+        """
+        Sorts the nodes in a min heap by their fitness.
+        """
+
+        if self.heap is None:
+            self.build_heap_from_dict()
+        else:
+            self.heap.sort()
+
+    def build_heap_from_dict(self):
+        """
+        Builds a heap from the dictionary that contains all the vertices.
+        """
+
+        self.heap = [node for key, node in self.var.items()]
+        heapq.heapify(self.heap)  # min heap
+
+    def random_swapping(self, tau):
+        """
+        Randomly swaps two vertices in different groups with a power law chance.
+        """
+
+        n, _ = self.adjacency.shape
+        distribution = self.truncated_power_law(tau, n)
+        k1 = distribution.rvs()
+        node1 = self.heap[k1 - 1]
+        grouping1 = node1.grouping
+        k2 = distribution.rvs()
+        while self.heap[k2 - 1].grouping == grouping1:
+            k2 = distribution.rvs()
+        node2 = self.heap[k2 - 1]
+        tmp = node1.grouping
+        node1.grouping = node2.grouping
+        node2.grouping = tmp
+        return node1, node2
+
+    def truncated_power_law(self, tau, m):
+        """
+        Generates the distribution according to a power law controlled by
+        parameter tau.
+        """
+
+        x = np.arange(1, m + 1, dtype='float')
+        pmf = 1 / x**tau
+        pmf /= pmf.sum()
+        return stats.rv_discrete(values=(range(1, m + 1), pmf))
+
+    def extremal_optimization(self, tau):
+        """
+        The Extremal Optimization algorithm.
+        """
+
+        self.initialize_grouping()
+        self.sorting_by_fitness()
+        n, _ = self.adjacency.shape
+        t_max = 20 * n  # TODO: customize const 1 << A << n
+        for i in range(t_max):
+            print("ROUND {}".format(i))
+            node1, node2 = self.random_swapping(tau)
+            node1.update_fitness()
+            node2.update_fitness()
+            for node in node1.nbrs:
+                node.update_fitness()
+            for node in node2.nbrs:
+                node.update_fitness()
+            self.sorting_by_fitness()
+        result = [node.grouping for key, node in self.var.items()]
+        return result
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import numpy as np
 import belief_propagation as bp
 import propagation_graph as graph
+from extremal_optimization import EONode, EOGraph
 
+# # for belief propagation
+# n = 50
+# q = 2
+# a = .2
+# b = .2
+# A, ground_truth, p_in, p_out = bp.select_model("const-sbm", n, a, b)
+# p = bp.edge_prob_mat(1, 0)
+# g = graph.Graph(A, 2, p)
+# g.construct_from_adj()
+# marginals = g.marginals(maxsteps=100)
+# print("MARGINALS:\n{}".format(marginals))
 
-n = 50
+# for extremal optimization
+n = 500
 q = 2
-a = .2
-b = .2
-A, ground_truth, p_in, p_out = bp.select_model("const-sbm", n, a, b)
-p = bp.edge_prob_mat(1, 0)
-g = graph.Graph(A, 2, p)
+a = 3
+b = 10
+tau = 1.4
+A, ground_truth, p_in, p_out = bp.select_model("sparse-sbm", n, a, b)
+p = bp.edge_prob_mat(p_in, p_out)
+g = EOGraph(A, 2, p)
 g.construct_from_adj()
-marginals = g.marginals(maxsteps=100)
-print("MARGINALS:\n{}".format(marginals))
+result = g.extremal_optimization(tau)
+print(result)
